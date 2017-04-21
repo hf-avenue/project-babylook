c63ee3538318e2c84dea6a2e48bc6423852aabc6
hf-avenue <hasidume.factory@gmail.com>
2017-04-21 12:48:49 +0900

    templateに画像投稿用フォームとmodelに画像種類認識を組み込んだ TODO:画像とサムネのローカル保存、サムネの一覧画面反映

--- src/Model/Table/ArticlesTable.php
+++ src/Model/Table/ArticlesTable.php
@@ -20,13 +20,27 @@ class ArticlesTable extends Table {
 
     public function validationDefault(Validator $validator)
     {
-        // TODO:実テーブル、バリデーション、templateに画像アップを実装
+
         $validator
             ->notEmpty('title')
             ->requirePresence('title')
             ->notEmpty('body')
             ->requirePresence('body');
-
+        // 画像フォーマットチェック20170421
+        $validator
+            ->allowEmpty('img_ext')
+            ->add('img_ext', ['list' => [
+                'rule' => ['inList', ['jpg','jpeg', 'png', 'gif']],
+                'message' => '投稿可能な画像はjpg, png, gif のみアップロード可能です.',
+            ]]);
+        // 画像フォーマットチェック20170421
+        $validator
+            ->integer('img_size')
+            ->allowEmpty('img_size')
+            ->add('img_size', 'comparison', [
+                'rule' => ['comparison', '<', 10485760],
+                'message' => 'ファイルが大きすぎます(MaxSize:10M)',
+            ]);
         return $validator;
     }
 
