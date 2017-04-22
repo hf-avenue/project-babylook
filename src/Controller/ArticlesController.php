7af197d8f732ba273cd891f550be55f17e0f8641
hf-avenue <hasidume.factory@gmail.com>
2017-04-23 02:30:24 +0900

    画像をwebrootにアップロード完了 todo リネームとサムネイル

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -10,6 +10,7 @@
 namespace App\Controller;
 
 use App\Controller\AppController;
+use Cake\Routing\Router;
 
 class ArticlesController extends AppController {
     // 一覧表示
@@ -40,6 +41,21 @@ class ArticlesController extends AppController {
             $article = $this->Articles->patchEntity($article, $this->request->getData());
             // 投稿者IDを追記
             $article->user_id = $this->Auth->user('id');
+            // ファイル名を転送 todo:rename
+
+
+$file_status = $article->img;
+
+            move_uploaded_file($file_status['tmp_name'], WWW_ROOT."/img/deliverable/" .$file_status['name']);
+
+
+
+
+
+
+
+
+
             if ($this->Articles->save($article)) {
                 $this->Flash->success(__('Your article has been saved.'));
                 return $this->redirect(['action' => 'index']);
