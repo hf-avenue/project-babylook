3e74fa32d1cdbaff34c50441675fe6bd8a5b4953
hf-avenue <hasidume.factory@gmail.com>
2017-06-14 11:15:31 +0900

    成功したミッションは記録され、表示も可能になる

--- src/Controller/MypagesController.php
+++ src/Controller/MypagesController.php
@@ -10,6 +10,8 @@
 namespace App\Controller;
 
 use App\Controller\AppController;
+use App\Model\Table\MissionMastersTable;
+use App\Model\Table\UserMissionStatusesTable;
 
 
 class MypagesController extends AppController {
@@ -31,6 +33,8 @@ class MypagesController extends AppController {
         $this->loadModel('Scores');
         $this->loadModel('Trophies');
         $this->loadModel('Users');
+        $this->loadModel('MissionMasters'); //TODO:後でモデル定義　2017/06/09
+        $this->loadModel('UserMissionStatuses'); //TODO:後でモデル定義　2017/06/09
 
         // ユーザー名取得
         $users = $this->Users->find('all', array('conditions' => array('Users.id' =>$user_id)));
@@ -54,6 +58,15 @@ class MypagesController extends AppController {
         // トロフィー一覧を取得
         $trophies = $this->Trophies->find('all');
         $this->set('trophies',$trophies);
+
+        // ここからミッションのフラグ確認追加です todo:アカウント作成時に最初のミッションを作成　2017/06/09 イイネと同じで別取得した方が早い
+        // 結合させたミッション進行度とミッションマスタを出してあげる
+        $missions = $this->UserMissionStatuses->find('all', array('conditions'=>array('UserMissionStatuses.user_id' => $user_id,)))->contain(['MissionMasters']);
+
+        $this->set('missions',$missions);
+
+
+
 
         // ここまで
 
---
+++ src/Model/Table/UserMissionStatusesTable.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 2017/03/10
+ * Time: 17:45
+ */
+
+namespace App\Model\Table;
+
+use Cake\ORM\Table;
+
+
+class UserMissionStatusesTable extends Table {
+
+
+    public function initialize(array $config)
+    {
+        // UsersテーブルのidとArticlesテーブルのuser_idは結合の為のキー
+        $this->belongsTo('MissionMasters')->setForeignKey('user_id')->setJoinType('INNER');
+        $this->addBehavior('Timestamp');
+    }
+
+
+
+
+
+
+}
